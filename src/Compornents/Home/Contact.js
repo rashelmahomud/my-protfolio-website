94bd8207c81b8a9398d6be5792f8e7b289d343fd
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-06-09 13:56:09 +0600

    added sent mail

--- src/Compornents/Home/Contact.js
+++ src/Compornents/Home/Contact.js
@@ -11,9 +11,9 @@ const Contact = () => {
                     </div>
 
                     <form method="POST" action="https://formsubmit.co/rashelmahmudraj1998@gmail.com" enctype="multipart/form-data" className='grid justify-items-center'>
-                        <input className='w-80 rounded-lg p-3' type="text" placeholder='Email Address' /> <br />
-                        <input className='w-80 rounded-lg p-3' type="text" placeholder='subject' /> <br />
-                        <textarea className='w-80 rounded-lg p-3' name="" id="" cols="40" rows="8" placeholder='your massage'></textarea>
+                        <input className='w-80 rounded-lg p-3' name='email' type="text" placeholder='Email Address' /> <br />
+                        <input className='w-80 rounded-lg p-3' name='text' type="text" placeholder='subject' /> <br />
+                        <textarea className='w-80 rounded-lg p-3' name="message" id="" cols="40" rows="8" placeholder='your massage'></textarea>
                         <button type="submit" className='btn btn-primary mt-5 text-white'>Send Test</button>
                     </form>
 
