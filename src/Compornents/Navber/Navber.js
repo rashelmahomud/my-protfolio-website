d182c282bd486960436c71469b04e877f5f350b2
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-06-10 20:02:40 +0600

    added details image

--- src/Compornents/Navber/Navber.js
+++ src/Compornents/Navber/Navber.js
@@ -10,7 +10,7 @@ const Navber = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link to="/home">Home</Link></li>
+                        <li><Link to="/">Home</Link></li>
                         <li><Link to="/blogs">Blogs</Link></li>
                         <li><a target="_blank" href='https://web.facebook.com/messages/t/100015152085518/'>Hire Me </a></li>
                         <li><Link to="/contact">Contact</Link></li>
@@ -21,7 +21,7 @@ const Navber = () => {
             </div>
             <div className="navbar-end hidden lg:flex">
                 <ul className="menu menu-horizontal p-0 font-bold">
-                    <li><Link to="/home">Home</Link></li>
+                    <li><Link to="/">Home</Link></li>
                     <li><Link to="/blogs">Blogs</Link></li>
                     <li><a target="_blank" href='https://web.facebook.com/messages/t/100015152085518/'>Hire Me</a></li>
                     <li><Link to="/contact">Contact</Link></li>
--- src/Compornents/Protfolio/ProtfolioDetails.js
+++ src/Compornents/Protfolio/ProtfolioDetails.js
@@ -4,19 +4,35 @@ import { useParams } from 'react-router-dom';
 const ProtfolioDetails = () => {
     const { detailsId } = useParams();
     const [service, setService] = useState([]);
-    useEffect( () => {
+    useEffect(() => {
         fetch("/details.json")
-        .then(res => res.json())
-        .then(data => setService(data) )
-    } ,[])
- 
-  const newService = service.filter(s => s._id == detailsId)
-//   console.log(newService[0]);
+            .then(res => res.json())
+            .then(data => setService(data))
+    }, [])
+
+    const newService = service.filter(s => s._id == detailsId)
+    //   console.log(newService[0]);
     return (
         <div>
-            <h1 className='text-2xl'>Service Details {detailsId}</h1>
-            <h1>name {newService[0]?.name}</h1>
-            <img src={newService[0]?.img}></img>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+
+                <div class="card w-96 bg-base-100 shadow-xl my-20 mx-20">
+                    <figure><img src={newService[0]?.img} alt="Shoes" /></figure>
+                </div>
+                <div class="card w-96 bg-base-100 shadow-xl my-20 mx-20">
+                    <figure><img src={newService[0]?.img2} alt="Shoes" /></figure>
+                </div>
+                <div class="card w-96 bg-base-100 shadow-xl my-20 mx-20">
+                    <figure><img src={newService[0]?.img3} alt="Shoes" /></figure>
+                </div>
+
+            </div>
+            <div className='text-center'>
+                <h1 className='text-4xl'>Website Name: <span className='text-4xl text-primary font-bold'>{newService[0]?.name}</span></h1>
+                <p> Technology used: {newService[0]?.dec}</p>
+
+
+            </div>
         </div>
     );
 };
