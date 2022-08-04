28d546dc01187c9284e779d1030bdc6ee968c800
Md Rashel <rashelmahmudraj1998@gmail.com>
2022-08-05 00:59:25 +0600

    added counter

--- src/Compornents/Home/Skill.js
+++ src/Compornents/Home/Skill.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CountUp from 'react-countup';
 
 const Skill = () => {
+    const [counter, setCounter] = useState(0);
+
+    useEffect( () => {
+        const interval = setInterval(() => {
+
+            setCounter((counter) => counter + 1);
+
+        }, 100000);
+
+        return () => {
+            clearInterval(interval);
+        }
+    } ,[])
+
+
     return (
         <div>
             <div>
@@ -14,7 +30,7 @@ const Skill = () => {
                 <div className="card w-96 bg-slate-600 shadow-xl">
                     <div className="card-body">
                         <div className='text-center'>
-                            <div className="radial-progress font-bold border-primary text-white" style={{ "--value": 90 }}>90%</div>
+                            <div className="radial-progress font-bold border-primary text-white" style={{ "--value": 90 }}><CountUp start={0} end={90} duration={5} />%</div>
                             <p className='font-bold mt-3 pragraph-color'>HTML & CSS</p>
                         </div>
                     </div>
@@ -22,7 +38,7 @@ const Skill = () => {
                 <div className="card w-96 bg-slate-700 shadow-xl">
                     <div className="card-body">
                         <div className='text-center'>
-                            <div className="radial-progress font-bold border-primary text-white" style={{ "--value": 70 }}>70%</div>
+                            <div className="radial-progress font-bold border-primary text-white" style={{ "--value": 70 }}><CountUp start={0} end={70} duration={5} />%</div>
                             <p className='font-bold mt-3 pragraph-color'>JavaScript</p>
                         </div>
                     </div>
@@ -30,7 +46,7 @@ const Skill = () => {
                 <div className="card w-96 bg-slate-800 shadow-xl">
                     <div className="card-body">
                         <div className='text-center'>
-                            <div className="radial-progress font-bold border-primary text-white" style={{ "--value": 80 }}>80%</div>
+                            <div className="radial-progress font-bold border-primary text-white" style={{ "--value": 80 }}><CountUp start={0} end={80} duration={5} />%</div>
                             <p className='font-bold mt-3 pragraph-color'>React.JS</p>
                         </div>
                     </div>
@@ -38,7 +54,7 @@ const Skill = () => {
                 <div className="card w-96 bg-slate-900 shadow-xl">
                     <div className="card-body">
                         <div className='text-center'>
-                            <div className="radial-progress font-bold border-primary text-white" style={{ "--value": 70 }}>70%</div>
+                            <div className="radial-progress font-bold border-primary text-white" style={{ "--value": 70 }}><CountUp start={0} end={70} duration={5} />%</div>
                             <p className='font-bold mt-3 pragraph-color'>Node.JS</p>
                         </div>
                     </div>
